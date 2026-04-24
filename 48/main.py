cb8ee2d0f26e54457d6f2f87a5990c23bacb0487
Claudio Morales <climoralesg@gmail.com>
2026-04-23 23:12:44 -0400

    main: progress

---
+++ 48/main.py
@@ -0,0 +1,12 @@
+#48	Suma los números del 1 al 100.
+
+finalNumber=100
+
+initialNumber=1
+counter=0
+
+while initialNumber <=100:
+    counter+=initialNumber
+    initialNumber+=1
+
+print(f"{counter}")

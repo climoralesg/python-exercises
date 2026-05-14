f70f2b4a3ef30d134284a4f1c9b149e4ea832007
Claudio Morales <climoralesg@gmail.com>
2026-05-13 23:38:19 -0400

    main: progress

--- 59/main.py
+++ 59/main.py
@@ -1,12 +1,17 @@
+#59	Cuenta cuántos intentos hizo el usuario.
+
 #58	Juego de adivinar número con intentos.
 import random
 
+attemps = 0
 randomNumber = random.randint(1,10)
 print(f"Clave: {randomNumber}")
 while True:
+    attemps +=1
     number = int(input("Ingresa el numero"))
     if number==randomNumber:
         print("adivinaste!")
+        print(f"Numero de intentos {attemps}")
         break
     else:
         print("Fallaste, intenta nuevamente")

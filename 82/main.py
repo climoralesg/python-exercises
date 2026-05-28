d2a6fb89ed0f87b5bbe3d773eea65f0e6b9c9523
Claudio Morales <climoralesg@gmail.com>
2026-05-27 23:48:38 -0400

    main: progress

---
+++ 82/main.py
@@ -0,0 +1,5 @@
+#82	Muestra todos los elementos de una lista en una sola línea.
+
+lista=["hola", 1,2,"buenas", 1000]
+
+print(lista)
---
+++ 83/main.py
@@ -0,0 +1,9 @@
+#83	Calcula la suma total de una lista de números.
+
+list = [1,1,1,1,1,1]
+
+list = [1,1,1,1,1,1,"list"] #marca error ya que los elementos deben ser int
+
+result = sum(list)
+
+print(f"{result}")
